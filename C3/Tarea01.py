1dd549f64ac7d620e9cb544e72e66bea09e3293b
German <germonram@gmail.com>
2021-07-25 10:35:39 -0600

    cambios de interprete

---
+++ C3/Tarea01.py
@@ -0,0 +1,10 @@
+# Created by German Montero at 25/7/2021
+# Escribir un programa que pida al usuario una palabra
+# y la muestre por pantalla 10 veces
+def get_data() -> int:
+    hola: float
+    return
+
+
+if __name__ == '__main__':
+    print("Hola")
