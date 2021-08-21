32131acf8111f3df147559312fed8b3c940f9cd9
German <germonram@gmail.com>
2021-08-20 18:46:39 -0600

    Clase 15

---
+++ C15/Exap02.py
@@ -0,0 +1,10 @@
+# Created by German Montero at 20/8/2021
+class Usuario:
+    def __init__(self, nombre: str, clave: str):
+        self.__clave = clave
+        self.nombre = nombre
+
+
+if __name__ == '__main__':
+    usuario = Usuario("Roberto", "asdas")
+    print(usuario.nombre, usuario._Usuario__clave)
